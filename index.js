e506fb50c8315a1b27e71336cfe6f0642191f558
Zouhair.ETTARAK <ettarak.zouhair@gmail.com>
2023-04-25 16:05:34 +0100

    add search input and coding method inputSearch

--- index.js
+++ index.js
@@ -15,3 +15,21 @@ async function getdata() {
 
     document.getElementById('tbody').innerHTML = table;
 }
+
+function onSearch() {
+    var value = document.getElementById('searchInput').value;
+    var table = document.getElementById('table');
+    var tr = table.getElementsByTagName('tr');
+
+    for (let index = 0; index < tr.length; index++) {
+        let firstName = tr[index].getElementsByTagName('td')[0];
+        let lastName = tr[index].getElementsByTagName('td')[1];
+        if (firstName || lastName) {
+            if (firstName.innerHTML.toLowerCase().indexOf(value.toLowerCase()) > -1) {
+                tr[index].style.display = '';
+            } else {
+                tr[index].style.display = 'none';
+            }
+        }
+    }
+}
